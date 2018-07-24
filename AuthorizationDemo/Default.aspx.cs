c17fc75b4258637f28219dfeec067f6e0c04dea5
Lizet Pena De Sola <lipenade@microsoft.com>
2018-07-24 13:07:48 -0500

    Initial commit

---
+++ AuthorizationDemo/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AuthorizationDemo
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Secure Method
+        [PrincipalPermission(SecurityAction.Demand, Authenticated = true)]
+        protected void btnAuthorizedAction_Click(object sender, EventArgs e)
+        {
+            // do some secure stuff here.
+            // only authenticated users are allow to access this method
+        }
+
+        #endregion
+    }
+}
---
+++ AuthorizationDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuthorizationDemo.Global" Language="C#" %>
